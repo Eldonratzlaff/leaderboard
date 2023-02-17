b5f5b697ab9a4fc07c1b289a4d4efef135488872
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-02-17 15:19:45 -0300

    Change readme.md

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// const refresh = document.getElementById('refresh-btn');
+// refresh.addEventListener('click', refreshEvent);
+// const submitBtn = document.getElementById('submit-btn');
+// submitBtn.addEventListener('click', addEvent);
